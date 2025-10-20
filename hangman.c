74da7481e4e4d2dd39adf25dfce98d58106130ee
mnerelong <meika.milton@ucas-edu.net>
2025-10-20 19:44:07 +0000

    Co-authored-by: dharrmanfam <sushidontlie520@users.noreply.github.com> Co-authored-by: Merp <eliasperkins-jpg@users.noreply.github.com>

--- hangman.c
+++ hangman.c
@@ -1,1 +1,38 @@
+// MM CC EP Hangman
+//hgey yalllll r u guys gud
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
 
+int main(void){
+// Meika's work      vvvvvvv Charlie picked the word haha
+    char word[] = "JaredLeto";
+    char guess;
+    printf("Welcome to hangman, please enter a letter.\n");
+    scanf("%c", &guess);
+
+char user_vari;
+char chosen_word = 
+//Elias worked on
+    if (user_vari){
+        printf("You can vote!\n")
+    }else if (age == 16){ 
+        printf("You can get a lisence!!\n");
+    }else if (age == 15){ 
+        printf("You can get a permit!\n");
+    }else if (age >= 6){
+        printf(" you can go to school! :(\n");
+    }else {
+        printf(":( GAME OVER, YOU FAILED, LOSE, GIT GUD, L, BOO HOO ):\n");
+
+    }
+
+//Charlie worked on 
+
+
+
+
+
+
+    return 0; 
+}
