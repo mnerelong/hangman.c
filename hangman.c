87af2ada9fe4adb1b6f950aab89a9bd61a2dc744
mnerelong <meika.milton@ucas-edu.net>
2025-10-21 19:23:28 +0000

    Co-authored-by: dharrmanfam <sushidontlie520@users.noreply.github.com>

--- hangman.c
+++ hangman.c
@@ -1,38 +1,57 @@
 // MM CC EP Hangman
-//hgey yalllll r u guys gud
+// hgey yalllll r u guys gud
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
+#include <time.h>
+#include <stdlib.h>
 
-int main(void){
-// Meika's work      vvvvvvv Charlie picked the word haha
-    char word[] = "JaredLeto";
-    char guess;
-    printf("Welcome to hangman, please enter a letter.\n");
-    scanf("%c", &guess);
+int main(void)
+{
+    // Meika's work
+    char test_list[] = {"Idunno", "FivePebbles", "Loremipsum", "Help"};
+    srand(time(NULL));
+    int random = rand() %4 +0;
 
-char user_vari;
-char chosen_word = 
-//Elias worked on
-    if (user_vari){
+
+        char user_vari;
+    char chosen_word =
+        // Elias worked on
+        if (user_vari)
+    {
         printf("You can vote!\n")
-    }else if (age == 16){ 
+    }
+    else if (age == 16)
+    {
         printf("You can get a lisence!!\n");
-    }else if (age == 15){ 
+    }
+    else if (age == 15)
+    {
         printf("You can get a permit!\n");
-    }else if (age >= 6){
+    }
+    else if (age >= 6)
+    {
         printf(" you can go to school! :(\n");
-    }else {
+    }
+    else
+    {
         printf(":( GAME OVER, YOU FAILED, LOSE, GIT GUD, L, BOO HOO ):\n");
-
     }
 
-//Charlie worked on 
+    // Charlie worked on
+     if (age < 0) {
+        printf("Please enter a real age.\n");
+    } else if (age >= 18) {
+        printf("You can vote!\n");
+    } else if (age >= 16) {
+        printf("You can drive!\n");
+    } else if (age >= 15) {
+        printf("You can get ur permit!\n");
+    } else if (age >= 6) {
+        printf("You can go to school!\n");
+    } else {
+        printf("You cant do any of these.\n");
+    }
 
-
-
-
-
-
-    return 0; 
+    return 0;
 }
