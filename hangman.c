0ed3c453346011dd0fb51deef16bda5a602145cf
mnerelong <meika.milton@ucas-edu.net>
2025-10-22 19:17:23 +0000

    Co-authored-by: Merp <eliasperkins-jpg@users.noreply.github.com> Co-authored-by: dharrmanfam <sushidontlie520@users.noreply.github.com>

--- hangman.c
+++ hangman.c
@@ -8,9 +8,17 @@
 
 int main(void)
 {
-    // Meika's work
+
+    int hp = 6;
     char word[10];
     srand(time(NULL));
+    int attempt_uno;
+    int attempt_dos;
+    int attempt_tres;
+    int attempt_quatro;
+    int attempt_sinco;
+    int attempt_seis;
+
     int random = rand() %10 +1;
     if (random == 1){
         char* word = "build";
@@ -27,38 +35,43 @@ int main(void)
     printf("%s\n", word);
 
     char user_vari;
-    char chosen_word =
+    char chosen_word();
+    
         // Elias worked on
-        if (user_vari)
-
-        else (hp - 1)
-        print
+    if (hp <= 0)
     {
-        printf("You can vote!\n")
-    }
-    if (hp < 0)
-    {
-        printf(" you can go to school! :(\n");
+        printf(":( GAME OVER, YOU FAILED, LOSE, GIT GUD, RIP, L, BOO HOO ):\n");
     }
     else
     {
-        printf(":( GAME OVER, YOU FAILED, LOSE, GIT GUD, L, BOO HOO ):\n");
-    }
-
-    // Charlie worked on
-     if ( < ) {
-        printf("\n");
-    } else if ( >= ) {
-        printf("\n");
-    } else if ( >= ) {
-        printf("\n");
-    } else if ( >= ) {
-        printf("\n");
-    } else if ( >= ) {
-        printf("\n");
-    } else {
         printf("\n");
     }
 
+    if ()
+    {
+        printf(":( GAME OVER, YOU FAILED, LOSE, GIT GUD, RIP, L, BOO HOO ):\n");
+    }
+    else
+    {
+        printf("\n");
+    }
+
+
+// Charlie worked on
+
+}
+    if(guessedalready){
+    Guessedletters[numguessed++] = guess;
+    printf("u got it wrong. Try again: ");
+    for (int i = 0; i < numguessed; i++){
+	printf("%c", guesssedletters[i]);
+}
+        printf("\n");
+}   else if {
+	    printf("u alr guessed that letter\n");
+}
+}   else {
+        printf("thats correct!!!\n");
+}
     return 0;
 }
