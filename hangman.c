6d399043f2c729d5830795fe4a7450f963be54f2
mnerelong <meika.milton@ucas-edu.net>
2025-10-22 19:43:40 +0000

    Co-authored-by: dharrmanfam <sushidontlie520@users.noreply.github.com> Co-authored-by: Merp <eliasperkins-jpg@users.noreply.github.com>

--- hangman.c
+++ hangman.c
@@ -6,8 +6,7 @@
 #include <time.h>
 #include <stdlib.h>
 
-int main(void)
-{
+int main(void){
 
     int hp = 6;
     char word[10];
@@ -17,11 +16,11 @@ int main(void)
     int attempt_tres;
     int attempt_quatro;
     int attempt_sinco;
-    int attempt_seis;
 
+    // Meika did the "element of randomness".
     int random = rand() %10 +1;
     if (random == 1){
-        char* word = "build";
+        char* word = "build"; 
     } else if (random == 2) {
         char* word = "lovely";
     } else if (random == 3) {
@@ -31,47 +30,35 @@ int main(void)
     } else {
         char* word = "five";
     }
-
-    printf("%s\n", word);
-
-    char user_vari;
-    char chosen_word();
     
         // Elias worked on
-    if (hp <= 0)
+        
+    
+        if (hp <= 0)
     {
         printf(":( GAME OVER, YOU FAILED, LOSE, GIT GUD, RIP, L, BOO HOO ):\n");
     }
-    else
-    {
-        printf("\n");
-    }
-
-    if ()
-    {
-        printf(":( GAME OVER, YOU FAILED, LOSE, GIT GUD, RIP, L, BOO HOO ):\n");
-    }
-    else
-    {
-        printf("\n");
-    }
+    else if (hp > 0)
+        printf("YOU WIN!!!!!!!");
+    
 
 
 // Charlie worked on
+    char letters[] =  {};   	int guessed =0;
+	char word[] = "build";
+	int length = strlen(word);
+	char guess;
+	int correct = 0;
 
-}
-    if(guessedalready){
-    Guessedletters[numguessed++] = guess;
-    printf("u got it wrong. Try again: ");
-    for (int i = 0; i < numguessed; i++){
-	printf("%c", guesssedletters[i]);
-}
-        printf("\n");
-}   else if {
-	    printf("u alr guessed that letter\n");
-}
-}   else {
-        printf("thats correct!!!\n");
-}
-    return 0;
+	printf("please pick a letter: ");
+	scanf("%c", &guess);
+	for(int i = 0; i < length; i++){ 
+		if (word [i] == guess){
+        correct = 1;    
+        } else { 
+		correct = 0;
+    printf("u got it wrong pls try again: ")
+		break;
+        }
+    return 0; 
 }
